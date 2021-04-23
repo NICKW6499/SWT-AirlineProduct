c17ab33ea7266b427bbb566966d5a15700bc5405
Sabrina Kienholz <snkienholz2938@eagle.fgcu.edu>
2021-04-22 20:26:44 -0400

    GUI testing comments

--- Tests/GuiTestSuite/searchCustomerUITest.java
+++ Tests/GuiTestSuite/searchCustomerUITest.java
@@ -10,12 +10,26 @@ public class searchCustomerUITest {
     Main main;
     searchCustomer customer;
 
+
 @BeforeEach
     void setup(){
     main = new Main();
     customer = new searchCustomer();
     searchCustomer.txtcustid.setText("CS001");
 }
+
+    /**
+     * Test Case ID: GT#1
+     * Purpose: To test that fields properly get populated in the GUI when prompted with set texts.
+     * Test setup: Setup includes new instance of Main and searchCustomer.
+     * Test strategy: Input validation of GUI components
+     * Input: doClick()
+     * Expected Output: firstNamePopulates(): "nick"
+     *              lastNamePopulates(): "wright"
+     *              nickNumbPopulates(): "123"
+     *              genderPopulates(): "Male"
+     *              contactPopulates(): "123532"
+     **/
 @Test
     void firstNamePopulates(){
     searchCustomer.jButton4.doClick();
@@ -41,6 +55,14 @@ public class searchCustomerUITest {
         searchCustomer.jButton4.doClick();
         Assertions.assertEquals("123532", searchCustomer.txtcontact.getText());
     }
+
+    /**
+     * Test Case ID: GT#2
+     * Purpose: To test that menu items properly respond to button clicks.
+     * Test setup: Setup includes new instance of Main and searchCustomer.
+     * Test strategy: Positive testing
+     * Input: doClick()
+     **/
     @Test
     void menuItem1Test(){
     Main.jMenu1.doClick();
